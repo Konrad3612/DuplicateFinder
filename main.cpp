60f9c47bca470d76ddfcffa2717b4179d3370962
Konrad3612 <45296896+Konrad3612@users.noreply.github.com>
2019-02-07 23:15:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-//#include "Analyzer.h"
-//#include <iostream>
 int main(array<System::String^>^ args)
 {
 	DuplicateFinderv2::MainGUI form;
